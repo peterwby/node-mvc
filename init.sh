725c8aa2a2f31d1907d18f5ba0bfa2634a6e65ab
doris <doris@dorisdeMacBook-Pro.local>
2019-11-26 20:38:59 +0800

    修复bug

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp ./session_index.js ./node_modules/@adonisjs/session/src/Session/index.js -f
+\cp -rf ./patch/session_index.js ./node_modules/@adonisjs/session/src/Session/index.js
 echo "init finish"
 echo "you can type 'npm run dev' to run the project"
