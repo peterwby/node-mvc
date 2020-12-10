853f2a2f930d2a16789b7228e4f05ab490d12f80
doris <doris@dorisdeMacBook-Pro.local>
2020-12-10 18:09:52 +0800

    更新事务回滚的处理方式

--- app/Services/MemberService.js
+++ app/Services/MemberService.js
@@ -155,13 +155,10 @@ class Service extends BaseService {
           column.cellphone = body.cellphone
         }
         result = await memberTable.create(trx, column)
+        if (result.status === 0) {
+          throw new Error('新增失败')
+        }
       })
-      if (result.status === 0) {
-        return Util.end({
-          msg: '新增失败',
-          status: 0,
-        })
-      }
 
       return Util.end({})
     } catch (err) {
@@ -203,13 +200,11 @@ class Service extends BaseService {
           set: { login_pwd: await Hash.make(body.new_pwd) },
         }
         result = await memberTable.updateBy(trx, data)
+        if (result.status === 0) {
+          throw new Error('新密码修改失败')
+        }
       })
-      if (result.status === 0) {
-        return Util.end({
-          msg: '新密码修改失败',
-          status: 0,
-        })
-      }
+
       return Util.end({})
     } catch (err) {
       return Util.error({
@@ -259,13 +254,11 @@ class Service extends BaseService {
           where: [['member_id', '=', body.member_id]],
           set: { member_name: body.member_name, email: body.email, cellphone: body.cellphone, remark: body.remark, gender_id: body.gender_id },
         })
+        if (result.status === 0) {
+          throw new Error('保存失败')
+        }
       })
-      if (result.status === 0) {
-        return Util.end({
-          msg: '保存失败',
-          status: 0,
-        })
-      }
+
       return Util.end({})
     } catch (err) {
       return Util.error({
@@ -281,13 +274,11 @@ class Service extends BaseService {
       const { body } = ctx
       await Database.transaction(async (trx) => {
         result = await memberTable.deleteByIds(trx, body.ids)
+        if (result.status === 0) {
+          throw new Error('删除失败')
+        }
       })
-      if (result.status === 0) {
-        return Util.end({
-          msg: '删除失败',
-          status: 0,
-        })
-      }
+
       return Util.end({
         data: result.data,
       })
